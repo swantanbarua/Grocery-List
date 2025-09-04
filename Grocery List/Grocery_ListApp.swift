c4d10365f90bf27207b3334cb8d72c2aa894785e
Swantan Barua <work.swantan@gmail.com>
2025-09-04 12:51:40 +0530

    Initial Commit

---
+++ Grocery List/Grocery_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_ListApp.swift
+//  Grocery List
+//
+//  Created by Swantan Barua on 04/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
