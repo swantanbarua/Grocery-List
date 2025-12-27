72a266b5622bbab94b4e59915af2537e7fa6ca94
Swantan Barua <work.swantan@gmail.com>
2025-12-27 23:57:58 +0530

    Add SwiftData integration to ContentView

--- Grocery List/index/ContentView.swift
+++ Grocery List/index/ContentView.swift
@@ -9,6 +9,12 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    
+    // MARK: - PROPERTIES
+    @Environment(\.modelContext) private var modelContainer
+    @Query private var items: [Item]
+    
+    // MARK: - BODY
     var body: some View {
         VStack {
             Image(systemName: "globe")
