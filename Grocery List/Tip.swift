6b0d86fa9c13954fe866b83985aaecc3f5fe1462
Swantan Barua <work.swantan@gmail.com>
2026-01-24 23:08:24 +0530

    Add Tip struct for essential foods overlay
    Introduces a new ButtonType struct conforming to Tip, providing a title, message, and image for overlay items in the shopping list.

---
+++ Grocery List/Tip.swift
@@ -0,0 +1,17 @@
+//
+//  Tip.swift
+//  Grocery List
+//
+//  Created by Swantan Barua on 24/01/26.
+//
+
+import Foundation
+import TipKit
+
+struct ButtonType: Tip {
+    
+    // MARK: - PROPERTIES
+    var title = Text("Essential Foods")
+    var message = Text("Add some overlay items to the shopping list")
+    var image = Image(systemName: "info.circle")
+}
